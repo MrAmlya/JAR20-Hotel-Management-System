094b11d6bad34201dafa8517e138b6df63ccfe2f
Amol Bhadane <asbhadane21@gmail.com>
2020-03-28 03:10:29 +0530

    First Commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\palak\downloads\hotel-management-system-master\hotel-management-system-master\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
